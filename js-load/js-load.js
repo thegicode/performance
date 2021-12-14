d5746503fbe020815c674de68d13f4e8a8a0e505
thegi.code <thegi.code@gmail.com>
2021-12-14 21:15:17 +0900

    Update js, css performance

--- js-load/js-load.js
+++ js-load/js-load.js
@@ -1,13 +1,1 @@
-
-
-function fn(){
-    const fragment = new DocumentFragment
-    for(let i = 0 ; i < 100 ; i++) {
-        const node = document.createElement('p')
-        node.textContent = i
-        fragment.appendChild(node)
-    }
-    document.querySelector('#log').appendChild(fragment)
-}
-
-fn()
+console.log('js-load')
